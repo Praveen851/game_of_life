14ad355fbd303dd5d045a054164a6fd6e0b1f3bf
praveen babu <rpraveenbabu851@gmail.com>
2022-05-23 15:25:50 +0530

    added class Colors and declared string variable to represent the colors

---
+++ view/Colors.java
@@ -0,0 +1,7 @@
+package view;
+
+public class Colors {
+    public static final String TEXT_WHITE = "\u001B[37m";
+    public static final String TEXT_YELLOW = "\u001B[33m";
+    public static final String TEXT_RESET = "\u001B[0m";
+}
