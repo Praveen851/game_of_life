02e173ab121730c229327d210ebe129f404ab00f
praveen babu <rpraveenbabu851@gmail.com>
2022-05-23 18:03:53 +0530

    added a for loop to iterate over surrounding and counted the number of alive cells surrounding the curr cell

--- controller/Rules.java
+++ controller/Rules.java
@@ -1,11 +1,16 @@
 package controller;
-
 import model.Board;
-
 public class Rules {
-    public static int[] surroudingRow = {-1, -1, -1, 0, 1, 1, 1, 0};
-    public static int[] surroudingCol = {-1, 0, 1, 1, 1, 0, -1, -1};
+    public static int[] surroudingRows = {-1, -1, -1, 0, 1, 1, 1, 0};
+    public static int[] surroudingColumns = {-1, 0, 1, 1, 1, 0, -1, -1};
+    private static int aliveCounter = 0;
     public static void modify(Board board, int currentRow, int currentCol){
-        
+        for(int index = 0; index < 8; index++){
+            int surroudingRow = surroudingRows[index] + currentRow;
+            int surroudingColumn = surroudingColumns[index] + currentCol;
+            if(board.grid[surroudingRow][surroudingColumn] == 1){
+                aliveCounter++;
+            } 
+        }
     }
 }
