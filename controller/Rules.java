aaaa1731f9b4bd25927b23edafaa2ea5c5b76f6d
praveen babu <rpraveenbabu851@gmail.com>
2022-05-23 17:51:52 +0530

    created class rules and declared surrounding row and col. Defined modify method

---
+++ controller/Rules.java
@@ -0,0 +1,11 @@
+package controller;
+
+import model.Board;
+
+public class Rules {
+    public static int[] surroudingRow = {-1, -1, -1, 0, 1, 1, 1, 0};
+    public static int[] surroudingCol = {-1, 0, 1, 1, 1, 0, -1, -1};
+    public static void modify(Board board, int currentRow, int currentCol){
+        
+    }
+}
