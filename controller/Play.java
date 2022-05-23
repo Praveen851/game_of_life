11d2653bfd245aca1404976ec7de23a10b11c6d8
praveen babu <rpraveenbabu851@gmail.com>
2022-05-23 17:52:29 +0530

    added loop for iteration

--- controller/Play.java
+++ controller/Play.java
@@ -5,5 +5,13 @@ import model.Board;
 public class Play {
     public static void playGame(Board board){
         int counter = 0;
+        while(counter < 50){
+            for(int row = 0; row < board.R; row++){
+                for(int col = 0; col < board.C; col++){
+                    Rules.modify(board, row, col);
+                }
+            }
+            counter++;
+        }
     }   
 }
