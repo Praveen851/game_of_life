ad2ba5f23c34da47518b50778b811b0382f8ccc4
praveen babu <rpraveenbabu851@gmail.com>
2022-05-23 18:53:22 +0530

    added code to display the grid with different colors for dead and alive cells

--- view/Display.java
+++ view/Display.java
@@ -1,5 +1,15 @@
 package view;
 
+import model.Board;
+
 public class Display {
-    
+    public static void printGrid(Board board){
+        for(int row = 0; row < board.R; row++){
+            for(int col = 0; col < board.C; col++){
+                if(board.grid[row][col] == 0)System.out.print(Colors.TEXT_WHITE + "#"+ Colors.TEXT_RESET);
+                else System.out.print(Colors.TEXT_YELLOW + "#"+ Colors.TEXT_RESET);
+            }
+            System.out.println();
+        }
+    }
 }
