fc6c3c818736a06ac10c2c7820e192cae11a1942
praveen babu <rpraveenbabu851@gmail.com>
2022-05-23 18:42:58 +0530

    modifed the life of cells based on rules

--- controller/Rules.java
+++ controller/Rules.java
@@ -1,10 +1,11 @@
 package controller;
+
 import model.Board;
-import java.util.ArrayList;
 public class Rules {
     
-    public static void modify(Board board, int currentRow, int currentCol){
-        int count = Count.getCount(board, currentRow, currentCol);
-        
+    public static void modify(Board board){
+        for(int index = 0; index < Play.tracker.size(); index++){
+            board.grid[Play.tracker.get(index).get(0)][Play.tracker.get(index).get(1)] = board.grid[Play.tracker.get(index).get(0)][Play.tracker.get(index).get(1)] == 1? 0:1;
+        }
     }
 }
